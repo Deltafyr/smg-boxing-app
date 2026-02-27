f75d99cc741e6389de9550b925d9d37f20980fbd
Deltafyr <armand.freyssinet@gmail.com>
2026-02-27 16:28:10 +0100

    MAJ avec Albedo

--- s.m.g-boxing-app/src/store/useSystemStore.ts
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand';
-interface SystemState { tokenStatus: string; isProcessing: boolean; consoleOutput: string; callCortex: () => Promise<void>; setConsoleOutput: (msg: string) => void; }
-export const useSystemStore = create<SystemState>((set) => ({
-  tokenStatus: 'DISCONNECTED', isProcessing: false, consoleOutput: '',
-  callCortex: async () => { set({ isProcessing: true }); setTimeout(() => set({ isProcessing: false, consoleOutput: 'Cortex Link Stabilisé.' }), 1000); },
-  setConsoleOutput: (msg) => set({ consoleOutput: msg })
-}));
